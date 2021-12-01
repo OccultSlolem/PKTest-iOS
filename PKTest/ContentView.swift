cd06db6ebaf648b17e576ab1c13d91c64e14370d
Ethan Hanlon <ehanlon19@gmail.com>
2021-12-01 02:45:18 -0800

    Initial Commit

---
+++ PKTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PKTest
+//
+//  Created by Ethan Hanlon on 12/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PKTest/PKTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKTestApp.swift
+//  PKTest
+//
+//  Created by Ethan Hanlon on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PKTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
